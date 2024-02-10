211b54b66ecb862fac39aa1dc023fb54f7f306df
rivers07-maker <riosandres91@gmail.com>
2024-02-10 17:18:46 -0400

    Swiper agregado

---
+++ script.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  //   effect: fade,
+  //   autoplay {
+  //   delay: 1000
+  // },
+  effect: "fade",
+  autoplay: {
+    delay: 2000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+
+
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+});
+
+
