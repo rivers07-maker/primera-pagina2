0f0961df292ae26704f5e974798edfe3ed2f4be4
rivers07-maker <riosandres91@gmail.com>
2024-03-13 01:13:09 -0400

    Nuevos cambios

--- script.js
+++ script.js
@@ -2,24 +2,11 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  //   effect: fade,
-  //   autoplay {
-  //   delay: 1000
-  // },
   effect: "fade",
   autoplay: {
     delay: 2000,
   },
 
-
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
 });
 
 
